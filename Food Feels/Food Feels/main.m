0ca7075043b13548250e37bcdd204c76212e84bb
Baconthorpe <zabuhoff@gmail.com>
2014-03-04 15:43:05 -0500

    initial commit

---
+++ Food Feels/Food Feels/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Food Feels
+//
+//  Created by Ezekiel Abuhoff on 3/4/14.
+//  Copyright (c) 2014 Ezekiel Abuhoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZAAppDelegate class]));
+    }
+}
